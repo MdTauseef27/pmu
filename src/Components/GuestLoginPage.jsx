35904c4d2d124f821941078da0a68831b46b5015
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-01-29 11:16:50 +0530

    give some stylesheet

--- src/Components/GuestLoginPage.jsx
+++ src/Components/GuestLoginPage.jsx
@@ -1,16 +1,48 @@
 import React from "react";
 import imgLogo from "./Images/universitylogo.jpg";
-import { Container } from "react-bootstrap";
+import { Container, Form, Button, Card, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 export const GuestLoginPage = () => {
   return (
     <div>
+      <img
+        src={imgLogo}
+        alt="img"
+        style={{
+          width: "60px",
+          height: "60px",
+          marginTop: "30px",
+          marginLeft: "600px",
+        }}
+      />
+      <h2 style={{ textAlign: "center", color: "#586069" }}>Guest Login</h2>
       <Container>
-        <img
-          src={imgLogo}
-          alt="img"
-          style={{ width: "40px", height: "40px" }}
-        />
-        <h2>Guest Login</h2>
+        <Card>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Row>
+                <Form.Label style={{ marginLeft: "20px" }}>
+                  User Name or E-mail Address
+                </Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Row>
+              {/* <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text> */}
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Signin
+            </Button>
+          </Form>
+        </Card>
       </Container>
     </div>
   );
