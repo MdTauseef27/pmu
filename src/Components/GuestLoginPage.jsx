dfaa922f5567ccfa48b8c74266f2e0c80d66ae75
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-01-29 10:30:22 +0530

    added container

--- src/Components/GuestLoginPage.jsx
+++ src/Components/GuestLoginPage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import imgLogo from "./Images/universitylogo.jpg";
-
+import { Container } from "react-bootstrap";
 export const GuestLoginPage = () => {
   return (
     <div>
-      {/* <Container> */}
-      <img src={imgLogo} alt="img" style={{ width: "40px", height: "40px" }} />
-      <h2>Guest Login</h2>
-      {/* </Container> */}
+      <Container>
+        <img
+          src={imgLogo}
+          alt="img"
+          style={{ width: "40px", height: "40px" }}
+        />
+        <h2>Guest Login</h2>
+      </Container>
     </div>
   );
 };
